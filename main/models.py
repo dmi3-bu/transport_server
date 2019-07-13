3e4f1f21a825410197fbd284020a2f317fea7a9e
fixen94 <fixen94@gmail.com>
2019-07-13 14:44:46 +1000

    models update

--- main/models.py
+++ main/models.py
@@ -1,5 +1,7 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class User(AbstractUser):
@@ -16,7 +18,18 @@ class User(AbstractUser):
     # groups - связь
     # user_permissions - связь
     # Кастомные поля:
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    passport = models.IntegerField(max_length=10, null=False)
     middle_name = models.CharField(max_length=50, default='')
     date_of_birth = models.DateField(null=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True)
+    phone = models.CharField(max_length=10, null=False)
+
+
+class Ticket(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, null=False, on_delete=models.CASCADE, related_name='tickets')
+    activated = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    used_at = models.DateTimeField(null=True)
